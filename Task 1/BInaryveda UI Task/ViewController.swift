4054a071d198b5279077c2ca05289d65f29eed81
Vasim Khan <75602156+askvasim@users.noreply.github.com>
2021-08-21 11:11:21 +0530

    init

---
+++ Task 1/BInaryveda UI Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BInaryveda UI Task
+//
+//  Created by Vasim Khan on 8/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
